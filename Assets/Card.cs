6ec0cc60e2dd83ef1517a6944cdda8b89547df59
dlukash3@stu.vistula.edu.pl <dlukash3@stu.vistula.edu.pl>
2024-02-28 23:03:54 +0100

    im done

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 CreateAssetMenu
-public class Card : ScriptableObject
+    public class Card : ScriptableObject
 
 {
     public int cardId;
